fdef05989834297c05af7495cac8989caea40029
U.L.G.Maduwantha <u.l.g.maduwantha@gmail.com>
2026-05-18 21:54:13 +0530

    Add OrderStatus enum - order status definitions

--- src/main/java/com/bookstore/management/dto/OrderStatusForm.java
+++ src/main/java/com/bookstore/management/dto/OrderStatusForm.java
@@ -1,19 +1,13 @@
 // Order Management Module - U.L.G.Maduwantha - IT19010618
-package com.bookstore.management.dto;
-
-import com.bookstore.management.model.OrderStatus;
+    package com.bookstore.management.dto;
+    import com.bookstore.management.model.OrderStatus;
 import jakarta.validation.constraints.NotNull;
-
-public class OrderStatusForm {
-
-    @NotNull(message = "Status is required")
-    private OrderStatus status;
-
-    public OrderStatus getStatus() {
+    public class OrderStatusForm {
+        @NotNull(message = "Status is required")
+        private OrderStatus status;
+        public OrderStatus getStatus() {
         return status;
     }
-
-    public void setStatus(OrderStatus status) {
+        public void setStatus(OrderStatus status) {
         this.status = status;
-    }
-}
+    }}
